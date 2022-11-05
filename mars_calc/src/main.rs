10298ff31f15c3a9b6b33a2c7ab751a21389f663
Yohan Kim <yohan@yohanpro.com>
2022-11-05 13:53:39 +0900

    Rust ownership

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,7 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    let result = io::stdin().read_line(&mut input)
+    .expect()
+    println!("Weight on Mars, {}kg", calculate_weight_on_mars(result));
 }
 
-fn calculatre_weight_on_mars(weight: f32)-> f32 {
-    return 50.0;
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    const GRAVITY_ON_MARS: f32 = 3.711;
+    const GRAVITY_ON_EARTH: f32 = 9.81;
+    return weight * GRAVITY_ON_MARS / GRAVITY_ON_EARTH;
 }
+
+// 러스트에는 ownership이란 개념이 있음.
+// 1. 각 value는 variable에 의해 소유됨.
+// 2. owner라는게 스코프 내에서 유효하지 않게 되면, 해당 변수는 자동으로 deallocate됨.
+// 3. 소유자는 오직 하나만 존재한다. 
